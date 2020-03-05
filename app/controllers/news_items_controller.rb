6e84bf74951c353a48d6e864bb5eac1411369e2e
Augustin Grenné <augustin@bpart.be>
2020-03-05 11:00:20 +0100

    added destroy to new items actions. abstracted form for news items in partial

--- app/controllers/news_items_controller.rb
+++ app/controllers/news_items_controller.rb
@@ -1,5 +1,7 @@
 class NewsItemsController < ApplicationController
   
+  before_action :set_news_item, only:[:show, :edit, :update, :destroy]
+
   def index
     @news_items = NewsItem.all.order("created_at ASC")
   end
@@ -19,15 +21,12 @@ class NewsItemsController < ApplicationController
   end
 
   def show
-    @news_item =  NewsItem.find(params[:id])
   end
 
   def edit
-    @news_item = NewsItem.find(params[:id])
   end
 
   def update
-    @news_item = NewsItem.find(params[:id])
     if @news_item.update(news_item_params)
       flash[:notice] = "The news item was sucessfully updated."
       redirect_to news_item_path(@news_item)
@@ -36,7 +35,20 @@ class NewsItemsController < ApplicationController
     end
   end
 
+  def destroy
+    if @news_item.destroy
+      flash[:notice] = "News item was sucessfully deleted."
+      redirect_to news_items_path
+    else
+      flash[:notice] = "News item could not be deleted."
+      redirect_to news_items_path
+    end
+  end
+
 private
+  def set_news_item
+    @news_item = NewsItem.find(params[:id])
+  end
   def news_item_params
     params.require(:news_item).permit(:title, :body, :show)
   end
