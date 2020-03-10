34b8dd5a3940b8c665fdede853dbf04f800c6cad
Augustin Grenné <grenne.augustin@gmail.com>
2020-03-10 09:47:53 +0100

    added users table & model

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-`# This file is auto-generated from the current state of the database. Instead
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_092424) do
+ActiveRecord::Schema.define(version: 2020_03_10_083825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2020_03_03_092424) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean "show"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
