54c7d90a9e4c63a9e7d6155ea24a3ae397676673
Augustin Grenné <grenne.augustin@gmail.com>
2020-03-15 11:53:15 +0100

    added styles and functionality for admin

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,12 @@ Rails.application.routes.draw do
   get "filosofie", to: "pages#filosofie"
   get "literatuur", to: "pages#literatuur"
 
-  scope(path_names: { new: "nieuw" }) do
+  scope(path_names: { new: "new" }) do
     resources :news_items, path: "nieuws"
   end
 
   get "login", to: "sessions#new"
+  get "admin", to: "sessions#new"
   post "login", to: "sessions#create"
   get "logout", to: "sessions#destroy"
   
